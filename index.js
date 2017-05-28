6fd0c484ecfccf58e25d1d125b8e92c75ea95d62
PhilippeMA <philippe.mace@gmail.com>
2017-05-28 10:25:20 +0200

    Mise en forme
    Test des outils d'édition

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = function(sails) {
-    
+
     // Connexion aux ressources NETATMO
     var install = require('./lib/install.js');
     var uninstall = require('./lib/uninstall.js');
-       
+
     return {
         install: install,
         uninstall: uninstall
